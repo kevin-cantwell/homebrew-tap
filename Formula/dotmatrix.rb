17f40731cf4203d030b78ba291d6a34f4fdf9c10
Kevin Cantwell <kevin.cantwell@gmail.com>
2026-02-14 16:34:15 -0500

    Use on_arm/on_intel DSL for proper architecture detection

--- Formula/dotmatrix.rb
+++ Formula/dotmatrix.rb
@@ -5,17 +5,18 @@ class Dotmatrix < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/kevin-cantwell/dotmatrix/releases/download/v#{version}/dotmatrix-darwin-arm64.tar.gz"
       sha256 "9916001ffb87306c2d272d59f67ca12dc97c8cb9a4d01d99e849c0f489ce08dc"
-    else
+    end
+    on_intel do
       url "https://github.com/kevin-cantwell/dotmatrix/releases/download/v#{version}/dotmatrix-darwin-amd64.tar.gz"
       sha256 "6465e8fee68c2547b69fbbc175e917fe55e086bf500ed50901ef7e70a28ec13b"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/kevin-cantwell/dotmatrix/releases/download/v#{version}/dotmatrix-linux-amd64.tar.gz"
       sha256 "07afa94bd589291c70e5e65fe8aa2ab766eece0b7d618b61fc5a848f8d7afe34"
     end
